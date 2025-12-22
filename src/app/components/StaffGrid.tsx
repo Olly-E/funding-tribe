da4a2000c1c71cf5c1d4ae494168e291892dfd72
Olly-E <akinsnuel@gmail.com>
2025-12-22 10:00:41 +0100

    work on staff grid

--- src/app/components/StaffGrid.tsx
+++ src/app/components/StaffGrid.tsx
@@ -32,18 +32,19 @@ const StaffGrid = () => {
             />
             <div
               className={clsx(
-                " text-start  border-t border-t-black absolute bottom-0 pt-2 w-full left-0 bg-white z-1 transition-all duration-1000",
+                " text-start  border-t border-t-black absolute bottom-0 md:pt-2 w-full left-0 bg-white z-1 transition-all duration-1000",
                 active === index
                   ? "max-h-[300px]"
                   : "lg:max-h-[60px] max-h-[50px] group-hover:max-h-[300px]"
               )}
             >
-              <div className="flex gap-2 sm:gap-3 items-center px-2 md:px-5 py-2 text-[10px] md:text-xs lg:text-base">
-                <strong className="whitespace-nowrap">{data.name}</strong>{" "}
+              <div className="min-h-[50px] gap-2 sm:gap-3 items-center px-2 md:px-5 py-2 text-xs lg:text-base">
+                <strong className="">{data.name}</strong>{" "}
                 {data.position && (
                   <span className="text-[4px] sm:text-[10px]">&#9679;</span>
-                )}
-                <span className="whitespace-nowrap">{data.position}</span>
+                )}{" "}
+                <br className="block md:hidden" />
+                <span className="">{data.position}</span>
               </div>
               <p className="mt-2 px-2 md:px-5 pb-4 text-[10px] md:text-xs lg:text-base">
                 {data.history}
